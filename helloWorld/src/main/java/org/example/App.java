f260dbfd7ee638d5051dfeb161d01034a405e6d6
yu <yu>
2024-01-03 18:19:58 +0800

    bug 修改

--- helloWorld/src/main/java/org/example/App.java
+++ helloWorld/src/main/java/org/example/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! 修改HelloWorld项目提交并推送" );
-        System.out.println("dev ");
+        System.out.println("bug");
     }
 }
