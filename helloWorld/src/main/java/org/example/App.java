e70861936d21962a8c18c4b31d0fda1865148bfd
yu <yu>
2024-01-03 18:20:58 +0800

    master 修改

--- helloWorld/src/main/java/org/example/App.java
+++ helloWorld/src/main/java/org/example/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! 修改HelloWorld项目提交并推送" );
+        System.out.println("master");
         System.out.println("bug");
     }
 }
